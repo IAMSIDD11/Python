2cf9678b59b837de9b3f7a3ba63e8fbf7418d285
Siddharth <iamsidd11@gmail.com>
2024-03-03 00:58:17 +0800

    My first commit to Github

---
+++ LinearSearch.py
@@ -0,0 +1,18 @@
+
+myList = [27,19,23,32,84,2,55,14,16,42]
+
+ub = 9
+lb = 0 
+top = ub 
+Swap = True 
+
+while Swap and top > lb:
+    Swap = False
+    for x in range (lb,top):
+        if myList[x] > myList[x+1]:
+            temp=myList[x]
+            myList[x] = myList[x+1]
+            myList[x+1]=temp
+            Swap = True
+    top = top-1
+print(myList)
